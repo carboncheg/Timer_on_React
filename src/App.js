5dc9e517e131a9a19efb9d01824595dffe3893c5
Serge <garibzyanovsergei@gmail.com>
2019-02-17 02:33:29 +0500

    Timer on React.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,67 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class TimerWrapper extends React.Component {
+  constructor(props) {
+    super(props);
+    this.startTimer = this.startTimer.bind(this);
+    this.state = {
+      timeLeft: null,   // Указывает на оставшееся время. Изначально равен null
+      timer: null       // Отсылается на наш таймер. Изначально равен null
+    };
+  }
+
+  startTimer(timeLeft) {   // Вызывается при нажатии на button
+    clearInterval(this.state.timer);  // Избавляемся от запусков нескольких таймеров одновременно при нажатии на разные кнопки
+    let timer = setInterval(() => {
+      let timeLeft = this.state.timeLeft - 1;  // Отнимает 1 секунду от оставшегося времени
+      if (timeLeft === 0) {    // Если оставшееся время равно 0,
+        clearInterval(timer);  // очищаем таймер, чтоб таймер не уходил в минус
+      }
+      this.setState({
+        timeLeft: timeLeft   // timeLeft из строки 10 равен timeLeft из строки 17
+      });
+    }, 1000);
+    return this.setState({timeLeft: timeLeft, timer: timer});
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+    return(
+      <div>
+        <h2>Timer</h2>
+        <div>
+          <Button time = '5' startTimer = {this.startTimer}/>
+          <Button time = '10' startTimer = {this.startTimer}/>
+          <Button time = '15' startTimer = {this.startTimer}/>
+        </div>
+        <TimerDisplay timeLeft = {this.state.timeLeft}/>
+        <audio id = "end" preload = "auto" src = "../media/bell.wav"></audio>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+class Button extends React.Component {
+  handleStartTimer() {
+    return this.props.startTimer(this.props.time)
+  }
+  render() {
+    return <button onClick = {this.handleStartTimer.bind(this)}>
+      {this.props.time} секунд</button>
+  }
+}
+
+class TimerDisplay extends React.Component {
+  render() {
+    if (this.props.timeLeft === 0) {
+      document.querySelector('end').play()
+    }
+    else if (this.props.timeLeft === 0 || this.props.timeLeft === null) {
+      return <div></div>
+    }
+    return <h1>Осталось {this.props.timeLeft} секунд</h1>
+  }
+}
+
+
+export default TimerWrapper;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class TimerWrapper extends React.Component {
+    constructor(props) {
+      super(props);
+      this.startTimer = this.startTimer.bind(this);
+      this.state = {
+        timeLeft: null,   // Указывает на оставшееся время. Изначально равен null
+        timer: null       // Отсылается на наш таймер. Изначально равен null
+      };
+    }
+  
+    startTimer(timeLeft) {   // Вызывается при нажатии на button
+      clearInterval(this.state.timer);  // Избавляемся от запусков нескольких таймеров одновременно при нажатии на разные кнопки
+      let timer = setInterval(() => {
+        let timeLeft = this.state.timeLeft - 1;  // Отнимает 1 секунду от оставшегося времени
+        if (timeLeft === 0) {    // Если оставшееся время равно 0,
+          clearInterval(timer);  // очищаем таймер, чтоб таймер не уходил в минус
+        }
+        this.setState({
+          timeLeft: timeLeft   // timeLeft из строки 10 равен timeLeft из строки 17
+        });
+      }, 1000);
+      return this.setState({timeLeft: timeLeft, timer: timer});
+    }
+  
+    render() {
+      return(
+        <div>
+          <h2>Timer</h2>
+          <div>
+            <Button time = '5' startTimer = {this.startTimer}/>
+            <Button time = '10' startTimer = {this.startTimer}/>
+            <Button time = '15' startTimer = {this.startTimer}/>
+          </div>
+          <TimerDisplay timeLeft = {this.state.timeLeft}/>
+          <audio id = "end" preload = "auto" src = "https://cloclo38.datacloudmail.ru/weblink/view/3duA/7CX7gBsRp?etag=8F02018E6396842FBB4BB9C002F84142CE7A68EB&key=4cc4a88a885da867606de14ef7d44014d8db9ba0"></audio>
+        </div>
+      )
+    }
+  }
+  
+  class Button extends React.Component {
+    handleStartTimer() {
+      return this.props.startTimer(this.props.time)
+    }
+    render() {
+      return <button onClick = {this.handleStartTimer.bind(this)}>
+        {this.props.time} секунд</button>
+    }
+  }
+  
+  class TimerDisplay extends React.Component {
+    render() {
+      if (this.props.timeLeft === 0) {
+        document.getElementById("end").play()
+      }
+      if (this.props.timeLeft === 0 || this.props.timeLeft === null) {
+        return <div></div>
+      }
+      return <h1>Осталось {this.props.timeLeft} секунд</h1>
+    }
+  }
+  
+
+ReactDOM.render(<TimerWrapper />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
